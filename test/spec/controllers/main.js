a83d3f3404742874833010a104f0daf69cde0dc9
Jesus Castro <jcastro@nearsoft.com>
2014-05-15 14:56:32 -0700

    modified the search and add bars, and gave condition when trying to add empty todos

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: TodoCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('todoAngularApp'));
+  beforeEach(module('TodoCtrl'));
 
-  var MainCtrl,
+  var TodoCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    MainCtrl = $controller('TodoCtrl', {
       $scope: scope
     });
   }));
