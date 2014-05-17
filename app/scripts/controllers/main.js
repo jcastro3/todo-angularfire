013edbf680b7d0d611e683241ce785639c100331
Jesus Castro <jcastro@nearsoft.com>
2014-05-16 17:31:23 -0700

    fixed most of the jshint syntax errors

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,42 +1,50 @@
 'use strict';
 
 mytodo.controller('TodoCtrl', function TodoCtrl($scope, $firebase, $location) {
-	var url = 'https://resplendent-fire-2473.firebaseio.com/';
+    var url = 'https://resplendent-fire-2473.firebaseio.com/';
     var ref = new Firebase(url);
-	$scope.getTotal = function(){
-		return $scope.todos.length;
-	};
+    
+    ref.on('child_added', function(snapshot) {
+        var newList = snapshot.newTodoProyect();
+      });
 
-	$scope.removeTodo = function() {
-		angular.forEach($scope.todos.$getIndex(), function (index) {
-			if($scope.todos[index].done === true) {
-				$scope.todos.$remove(index);
-			}
-		});
-	};
-
-	$scope.addTodo = function(){
+    $scope.getTotal = function(){
+      return $scope.todos.length;
+    };
+    
+    $scope.removeTodo = function() {
+        angular.forEach($scope.todos.$getIndex(), function (index) {
+            if($scope.todos[index].done === true) {
+              $scope.todos.$remove(index);
+            }
+          });
+      };
+    
+    $scope.addTodo = function(){
         if(!$scope.todoElem){
-            return;
+          return;
         }
         var data = {text:$scope.todoElem, done:false};
-		$scope.todos.$add(data);
-		$scope.todoElem = "";
-	};
+        $scope.todos.$add(data);
+        $scope.todoElem = '';
+      };
 
-	$scope.getTodo = function(pathurl){
-		$location.path(pathurl);
-	};
+    $scope.getTodo = function(pathurl){
+      $location.path(pathurl);
+    };
 
     $scope.newTodoProyect = function(proyectName){
-        //connect to firebase and create new Repo for new TodoProyect
         //once new repo is created show new list of items on the todolist
         url += proyectName;
         var ref = new Firebase(url);
         $scope.todos = $firebase(ref);
+      };
+
+    $scope.selectedProyect = function(selected){
+
     };
     //This will bind the todos with the firebase database
-	$scope.todos = $firebase(ref);
+    $scope.todos = $firebase(ref);
   });
 
 
